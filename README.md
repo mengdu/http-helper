4a4507c138a830b6c704b8ba0346ea9ac8135710
v_yguiliu <v_yguiliu@tencent.com>
2018-07-20 19:55:55 +0800

    update pkg info

--- README.md
+++ README.md
@@ -1,18 +1,89 @@
 # http-helper
 
-http helper for node.
+Http request tool for node.
 
-> 暂不支持上传文件！！
 
 ```js
-const http = new HttpHepler()
+import MHttp from 'm-http'
+const http = new MHttp()
 
 http.get('https://github.com').then(res => {
   console.log(res.toString())
 })
 ```
 
-**interceptors**
+## API
+
+**new MHttp(options)** 创建一个请求对象http
+
+  + **options** `array` 可选 配置对象
+  + **options.urlEncode** `boolean` 是否编码url，默认true
+
+### http
+
++ **http.fetch(options)** 提交一个请求，返回promise
+
+  + **options** `object` 必须，请求参数配置
+  + **options.url** `string` 必须，路径
+  + **options.method** `string` 可选，请求类型，默认 GET
+  + **options.timeout** `string` 
+  + **options.headers** `object` 可选，请求头设置，key/val  
+  + **options.auth** `string`
+  + **options.agent** `string`
+  + **options.body** `object` 可选，含有body的提交，key/val
+  + **options.params** `object` 可选，构造查询参数，key/val
+
++ **http.get(url, options)**
++ **http.delete(url, options)**
++ **http.head(url, options)**
++ **http.post(url, options)**
++ **http.put(url, options)**
++ **http.patch(url, options)**
+
+### Events
+
+**start**
+
+开始发起请求，参数： `options`
+
+**end**
+
+响应结束，参数：'IncomingMessage'
+
+**error**
+
+请求过程存在错误
+
+
+**upload**
+
+ `multipart/form-data` 请求时触发
+
+
+### interceptors
+
+
+
+**请求中间件**
+
+```js
+http.request.use(function(options, next) {
+  // do
+  next()
+})
+```
+
+**响应中间件**
+
+```js
+http.response.use(function(options, next) {
+  // do
+  next()
+})
+```
+
+
+**e.g.**
 
 ```js
 
@@ -36,51 +107,53 @@ http.response.use(function (res, next) {
 })
 ```
 
-## API
 
-**new HttpHepler(options)** 创建一个请求对象http
-
-  + **options** `array` 可选 配置对象
-  + **options.urlEncode** `boolean` 是否编码url，默认true
-  + **options.bodyEncode** `boolean` body编码类型，默认utf8
-
-### http
-
-+ **http.fetch(options)** 提交一个请求，返回promise
-
-  + **options** `object` 必须，请求参数配置
-  + **options.url** `string` 必须，路径
-  + **options.method** `string` 可选，请求类型，默认 GET
-  + **options.timeout** `string` 
-  + **options.headers** `object` 可选，请求头设置，key/val  
-  + **options.auth** `string`
-  + **options.agent** `string`
-  + **options.body** `object` 可选，含有body的提交，key/val
-  + **options.params** `object` 可选，构造查询参数，key/val
-
-+ **http.get(url, options)**
-+ **http.delete(url, options)**
-+ **http.head(url, options)**
-+ **http.post(url, options)**
-+ **http.put(url, options)**
-+ **http.patch(url, options)**
-
-### interceptors
-
-**请求中间件**
+## application/x-www-form-urlencoded
 
 ```js
-http.request.use(function(options, next) {
-  // do
-  next()
+http.post('http://localhost:3000/api/all', {
+  params: {
+    page: 1,
+    pageSize: 12,
+    zh: '中文'
+  },
+  body: {
+    username: 'admin',
+    password: '123456'
+  }
+}).then(res => {
+  console.log('response:', res.body.toString())
+}).catch(err => {
+  console.log('response err:', err)
 })
 ```
 
-**响应中间件**
+## multipart/form-data
 
 ```js
-http.response.use(function(options, next) {
-  // do
-  next()
+import MHttp from 'm-http'
+import FormData from 'm-http/form-data'
+import File from 'm-http/file'
+
+const http = new MHttp()
+const form = new FormData()
+
+form.append('key', 'value')
+form.append('username', 'admin')
+form.append('password', '123456')
+form.append('userId', 1001)
+form.append('zh', '中文')
+form.append('data', JSON.stringify({name: 'admin', info: 'this is a test.', zh: '中文'}))
+form.append('text', new File({name: 'text.txt', buffer: Buffer.from('this is a test\r\n这是一个测试。')}))
+form.append('zip', new File({filePath: 'D:/Software/mysql-5.6.40-winx64.zip'}))
+
+console.log(form)
+
+http.post('http://localhost:3000/api/file', {
+  body: form
+}).then(res => {
+  console.log('response:', res.body.toString())
+}).catch(err => {
+  console.log('response err:', err)
 })
 ```
--- demo.js
+++ demo.js
@@ -1,11 +1,14 @@
-const HttpHepler = require('./dest').default
-const http = new HttpHepler()
+const MHttp = require('./dest').default
+const FormData = require('./dest/form-data').default
+const File = require('./dest/file').default
 
-http.request.use(function (options, next) {
-  console.log(1)
-  console.log(options)
-  next()
-})
+const http = new MHttp()
+
+// http.request.use(function (options, next) {
+//   console.log(1)
+//   console.log(options)
+//   next()
+// })
 // .use(function (options, next) {
 //   console.log(2)
 //   next()
@@ -58,104 +61,53 @@ http.request.use(function (options, next) {
 // }).catch(err => {
 //   console.log(err.message)
 // })
-const crypto = require('crypto')
-const fs = require('fs')
-const img = fs.readFileSync('./test/demo.png')
-// console.log(img)
-const boundary = '----' + crypto.randomBytes(4).toString('hex')
-// const boundary = 'AaB03x'
-// const body = `--${boundary}\r\nContent-Disposition: form-data; name="attr1"\r\n\r\n123\r\n--${boundary}--`
-const data = [
-  '\r\nContent-Disposition: form-data; name=\"attr1\"\r\n\r\n123\r\n',
-  // '\r\nContent-Disposition: form-data; name="attr2"\r\n\r\n123\r\n',
-  // '\r\nContent-Disposition: form-data; name="file"; filename="demo.js"\r\nContent-Type: text/javascript\r\n\r\n123\r\n',
-  // `\r\nContent-Disposition: form-data; name="file"; filename="demo.png"\r\nContent-Type: image/png\r\nContent-Transfer-Encoding: binary\r\n\r\n${img}\r\n`
-]
-const body = `--${boundary}${data.join('--' + boundary)}--${boundary}--`
 
 // --------------http-helper-----------
 
-http.post('http://localhost:3000/api/file', {
-  headers: {
-    'Content-Type': `multipart/form-data; boundary=${boundary}`,
-    // 'Content-Type': `multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW`,
-    // 'Content-Length': '7'
+http.on('start', options => {
+  console.log('start:\n', options)
+})
+
+http.on('upload', (item) => {
+  console.log('upload:\n', item)
+})
+
+// application/x-www-form-urlencoded
+http.post('http://localhost:3000/api/all', {
+  params: {
+    page: 1,
+    pageSize: 12,
+    zh: '中文'
   },
-  body: body,
-  // body: {
-  //   attr1: 123
-  // }
+  body: {
+    username: 'admin',
+    password: '123456'
+  }
 }).then(res => {
   console.log('response:', res.body.toString())
 }).catch(err => {
-  console.log('response err:', err.message)
+  console.log('response err:', err)
 })
 
 
+// multipart/form-data
+const form = new FormData()
 
-//-------------------node native----------
-// var xhr = require("http");
+form.append('key', 'value')
+form.append('username', 'admin')
+form.append('password', '123456')
+form.append('userId', 1001)
+form.append('zh', '中文')
+// form.append('data', JSON.stringify({name: 'admin', info: 'this is a test.', zh: '中文'}))
+form.append('text', new File({name: 'text.txt', buffer: Buffer.from('this is a test\r\n这是一个测试。')}))
+form.append('zip', new File({filePath: 'D:/Software/mysql-5.6.40-winx64.zip'}))
 
-// var options = {
-//   protocol: 'http:',
-//   "method": "POST",
-//   "hostname": 'localhost',
-//   "port": 3000,
-//   "path": '/api/file',
-//   "headers": {
-//     'Content-Type': `multipart/form-data; boundary=${boundary}`,
-//     'Transfer-Encoding': 'chunked'
-//     // 'Content-Type': 'multipart/form-data; boundary=----5b04e390'
-//     // 'Content-Type': 'application/x-www-form-urlencoded'
-//   }
-// };
+console.log(form)
 
-// var req = xhr.request(options, function (res) {
-//   var chunks = [];
-
-//   res.on("data", function (chunk) {
-//     chunks.push(chunk);
-//   });
-
-//   res.on("end", function () {
-//     var data = Buffer.concat(chunks);
-//     console.log(data.toString());
-//   });
-// });
-// console.log(body)
-// req.on('error', err => {
-//   console.log('error', err)
-// })
-// // req.write(body)
-// req.write(`--${boundary}`)
-// req.write(`${data.join('--' + boundary)}`)
-// req.write(`--${boundary}`)
-// req.write('\r\nContent-Disposition: form-data; name="file"; filename="demo.js"\r\nContent-Type: text/javascript\r\n\r\n')
-// req.write('this is a test\r\n这是一个测试。', 'utf8')
-// req.write('\r\n')
-// req.write(`--${boundary}`)
-// // req.write('\r\nContent-Disposition: form-data; name="img"; filename="demo.png"\r\nContent-Type: image/png\r\n\r\n')
-// // req.write(img, 'binary')
-// // req.write('\r\n')
-// // req.write(`--${boundary}--`)
-// // req.end();
-
-
-// const buf = fs.createReadStream('D:/Software/mysql-5.6.40-winx64.zip')
-
-// // buf.pipe(function () {
-// //   console.log(arguments)
-// // })
-// req.write('\r\nContent-Disposition: form-data; name="img"; filename="demo.zip"\r\nContent-Type: application/zip\r\n\r\n')
-// buf.on('data', chunk => {
-//   // console.log(chunk)
-//   req.write(chunk, 'binary')
-// })
-
-// buf.on('end', () => {
-//   console.log('end')
-//   req.write('\r\n')
-//   req.write(`--${boundary}--`)
-//   req.end();
-// })
-
+http.post('http://localhost:3000/api/file', {
+  body: form
+}).then(res => {
+  console.log('response:', res.body.toString())
+}).catch(err => {
+  console.log('response err:', err)
+})
